8dac85db81705ab77de386fa637ebff6953d8d17
1jammer1 <s.kulla4096@lakewoodcityschools.org>
2025-07-27 16:21:53 -0400

    fixes an error :3

--- scripts/customize_airootfs.sh
+++ scripts/customize_airootfs.sh
@@ -31,14 +31,14 @@ ln -sf /usr/lib/systemd/system/lightdm.service /etc/systemd/system/lightdm.servi
 
 # xfce stuff
 if command -v xfconf-query &>/dev/null; then
-  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Net/ThemeName -s "TWM-theme"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Net/IconThemeName -s "TWM"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Gtk/FontName -s "terminus Medium 11"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "Terminus Bold 11"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xfwm4 -p /general/theme -s "TWM-theme"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xfwm4 -p /general/title_font -s "terminus Bold 15"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xfwm4 -p /general/title_alignment -s "left"' || true
-  su - batarong -c 'dbus-launch xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /usr/share/backgrounds/TW.png' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Net/ThemeName -s "TWM-theme" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Net/IconThemeName -s "TWM" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Gtk/FontName -s "terminus Medium 11" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "Terminus Bold 11" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xfwm4 -p /general/theme -s "TWM-theme" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xfwm4 -p /general/title_font -s "terminus Bold 15" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xfwm4 -p /general/title_alignment -s "left" --create' || true
+  su - batarong -c 'dbus-launch xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /usr/share/backgrounds/TW.png --create' || true
 else
   echo "warning: xfconf-query not found. xfce settings might not work"
 fi
